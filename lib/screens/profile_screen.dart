db7458f7311fd130685ffae2cd47f874777700f3
suheerthedev <khansuheer1@gmail.com>
2024-09-12 14:12:41 +0500

    Starting Building Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:clothing_store_app/util/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconsax/iconsax.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,20 +8,26 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              Iconsax.emoji_sad4,
-              size: 100,
-            ),
-            Text(
-              """Profile Screen
-Building In Progress""",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+//             Icon(
+//               Iconsax.emoji_sad4,
+//               size: 100,
+//             ),
+//             Text(
+//               """Profile Screen
+// Building In Progress""",
+//               textAlign: TextAlign.center,
+//               style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+//             ),
+            SizedBox.square(
+              dimension: 120,
+              child: CircleAvatar(
+                child: SvgPicture.asset(AppImages.profileImageIcon),
+              ),
             ),
           ],
         ),
