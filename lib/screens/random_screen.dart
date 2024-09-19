55f40eed924beccf7a2eee7efe1e05c03d3b7c9f
suheerthedev <khansuheer1@gmail.com>
2024-09-19 20:58:09 +0500

    Added another screen

---
+++ lib/screens/random_screen.dart
@@ -0,0 +1,110 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:clothing_store_app/widgets/custom_app_bar_widget.dart';
+import 'package:clothing_store_app/widgets/custom_banner_widget.dart';
+import 'package:clothing_store_app/widgets/custom_category_slider.dart';
+import 'package:clothing_store_app/widgets/custom_product_grid_widget.dart';
+import 'package:clothing_store_app/widgets/custom_search_bar_widget.dart';
+import 'package:clothing_store_app/widgets/custom_section_heading_widget.dart';
+import 'package:clothing_store_app/widgets/custom_tabbar_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../util/app_colors.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  final CarouselSliderController _carouselController =
+      CarouselSliderController();
+  int _carouselCurrentIndex = 0;
+  List<String> promoBanners = [
+    'assets/images/promobanner1.svg',
+    'assets/images/promobanner2.svg',
+    'assets/images/promobanner3.svg',
+  ];
+
+  List filterList = ["All", "Newest", "Popular", "Men", "Women", "Kids"];
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.sizeOf(context);
+    return DefaultTabController(
+      length: filterList.length,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: AppColors.surfaceColor,
+          title: const CustomAppBarWidget(),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //Search Bar
+              CustomSearchBarWidget(size: size),
+              //Search Bar Ended
+              //Carousel Started
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 8),
+                child: CarouselSlider.builder(
+                  carouselController: _carouselController,
+                  options: CarouselOptions(
+                      viewportFraction: 1,
+                      height: size.height * 0.2,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _carouselCurrentIndex = index;
+                        });
+                      }),
+                  itemCount: promoBanners.length,
+                  itemBuilder: (context, index, realIndex) =>
+                      CustomBannerWidget(
+                          size: size, svgImageUrl: promoBanners[index]),
+                ),
+              ),
+              SizedBox(
+                child: AnimatedSmoothIndicator(
+                  activeIndex: _carouselCurrentIndex,
+                  count: promoBanners.length,
+                  effect: ScrollingDotsEffect(
+                      activeDotColor: AppColors.primaryIconColor,
+                      dotColor: AppColors.secondaryIconColor,
+                      dotHeight: 4,
+                      dotWidth: 25),
+                  onDotClicked: (index) {
+                    _carouselController.animateToPage(index);
+                  },
+                ),
+              ),
+              //Carousel Ended
+              //Category Slider Started
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    CustomSectionHeadingWidget(
+                        textColor: AppColors.primaryTextColor,
+                        showActionButton: true,
+                        title: "Category",
+                        buttonTitle: "See All",
+                        onPressed: () {}),
+                    const CustomCategorySlider(),
+                  ],
+                ),
+              ),
+              //Category Slider Started
+              //TabBar Started
+              CustomTabBarWidget(fliterList: filterList),
+              const CustomProductGridWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
